195b5f7aca08e93b8605929b4f29e0a20fdabc19
msd98 <mukeshdhanure0@gmail.com>
2023-02-26 20:14:12 +0530

    first comment

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ student_notes = [open(_file, encoding='utf-8').read()
                  for _file in student_files]
 
 
-def vectorize(Text): return TfidfVectorizer().fit_transform(Text).toarray()
-def similarity(doc1, doc2): return cosine_similarity([doc1, doc2])
+def vectorize(Text): 
+    return TfidfVectorizer().fit_transform(Text).toarray()
 
+def similarity(doc1, doc2): 
+    return cosine_similarity([doc1, doc2])
 
 vectors = vectorize(student_notes)
 s_vectors = list(zip(student_files, vectors))
@@ -22,6 +24,7 @@ def check_plagiarism():
         new_vectors = s_vectors.copy()
         current_index = new_vectors.index((student_a, text_vector_a))
         del new_vectors[current_index]
+
         for student_b, text_vector_b in new_vectors:
             sim_score = similarity(text_vector_a, text_vector_b)[0][1]
             student_pair = sorted((student_a, student_b))
